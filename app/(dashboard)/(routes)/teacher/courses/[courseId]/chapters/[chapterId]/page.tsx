f0ad7894b8b6cf755a68ffddbf9e5d02c8b69d5f
Enric <enric.carstens@gmail.com>
2023-10-10 14:35:22 +0200

    added mux player, chapter video form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
+import { ChapterVideoForm } from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({ params }: { params: { courseId: string; chapterId: string } }) => {
     const { userId } = auth();
@@ -88,6 +89,7 @@ const ChapterIdPage = async ({ params }: { params: { courseId: string; chapterId
                             Add a video
                         </h2>
                     </div>
+                    <ChapterVideoForm initialData={chapter} courseId={params.courseId} chapterId={params.chapterId} />
                 </div>
             </div>
         </div>
